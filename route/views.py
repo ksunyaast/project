1b6170edd2363baefdf2002640304a9f5cf46a3b
Kseniya Astanaeva <ksunyaast@mail.ru>
2019-11-07 22:02:14 +0700

    add changes

--- route/views.py
+++ route/views.py
@@ -6,19 +6,20 @@ def route_view(request):
     template = 'stations.html'
     routes = Route.objects.all().prefetch_related('stations')
     stations = None
+    center = {'x': 55, 'y': 37}
 
     route = request.GET.get('route')
     if route:
         stations = routes.get(name=route).stations.all()
+        stations_latitude = stations.order_by('latitude')
+        stations_longitude = stations.order_by('longitude')
+        x = (stations_latitude[0].latitude + stations_latitude[len(stations_latitude)-1].latitude) / 2
+        y = (stations_longitude[0].longitude + stations_longitude[len(stations_longitude) - 1].longitude) / 2
+        center = {
+            'x': x,
+            'y': y,
+        }
 
-    stations_latitude = stations.order_by('latitude')
-    stations_longitude = stations.order_by('longitude')
-    x = (stations_latitude[0].latitude + stations_latitude[len(stations_latitude)-1].latitude) / 2
-    y = (stations_longitude[0].longitude + stations_longitude[len(stations_longitude) - 1].longitude) / 2
-    center = {
-        'x': x,
-        'y': y,
-    }
 
     context = {
         'routes': routes,
